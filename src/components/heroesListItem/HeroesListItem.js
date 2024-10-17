04477edadb6cc20a7cc84fc2dd444819a8ab246f
Ruslan <example@gmail.com>
2024-10-17 14:37:21 +0300

    Changing images

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -23,7 +23,7 @@ const HeroesListItem = ({ name, description, element, onDelete }) => {
       className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}
     >
       <img
-        src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg"
+        src="https://www.kindpng.com/picc/m/52-526072_unknown-character-hd-png-download.png"
         className="img-fluid w-25 d-inline"
         alt="unknown hero"
         style={{ objectFit: "cover" }}
